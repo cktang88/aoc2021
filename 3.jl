19fd7e5346df9772be83a4b1ccca09910d7edc9a
cktang88 <arctangent20@yahoo.com>
2021-12-04 14:22:43 -0600

    i

--- 3.jl
+++ 3.jl
@@ -1,18 +1,50 @@
 using DelimitedFiles
 
 
+
 function solve()
-    arr = readdlm("./3.txt", String) # reads into matrix
-    gamma, epsilon = "", ""
-    for j in 1:length(arr[1])
-        col = map(x -> x[j], arr)
-        cnt = count(x -> x == '1', col)
-        # println(cnt)
-        gamma *= cnt > length(col) / 2 ? "1" : "0"
-        epsilon *= cnt < length(col) / 2 ? "1" : "0"
-    end
-    println(parse(Int, gamma, base=2) * parse(Int, epsilon, base=2))
+    arr = readdlm("./3.txt", String)
+    gamma, epsilon = aggregate_bits(arr)
+    println(parse_bin(gamma) * parse_bin(epsilon))
+
+    # part 2
+    bnum = recurse_filter(arr)
+    o2 = parse_bin(bnum)
+    bnum = recurse_filter(arr, use_gamma=false)
+    co2 = parse_bin(bnum)
+    println(o2 * co2)
 end
 
-print(solve())
+function aggregate_bits(arr)
+    gamma, epsilon = "", ""
+    numcols = length(arr[1])
+    for j in 1:numcols
+        col = map(x -> x[j], arr)
+        numrows = length(col)
+        cnt = count(x -> x == '1', col)
+
+        gamma *= cnt >= numrows / 2 ? "1" : "0"
+        epsilon *= cnt < numrows / 2 ? "1" : "0"
+    end
+    return gamma, epsilon
+end
+
+function parse_bin(n::String)
+    return parse(Int, n, base=2)
+end
+
+function recurse_filter(origArray;use_gamma=true)
+    arr = copy(origArray)
+    for j in 1:length(arr[1])
+        gamma, epsilon = aggregate_bits(arr)
+        arr = filter(x -> x[j] == (use_gamma ? gamma[j] : epsilon[j]), arr)
+        if length(arr) == 1
+            return arr[1]
+        elseif length(arr) == 0
+            throw(ErrorException("Array now empty..."))
+        end
+    end
+end
+
+solve()
     
