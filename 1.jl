66ed292e47c7041bbf0f1540b01da79fc298d8a6
cktang88 <arctangent20@yahoo.com>
2021-12-04 11:41:55 -0600

    i

---
+++ 1.jl
@@ -0,0 +1,17 @@
+using DelimitedFiles
+
+
+function solve()
+    arr = readdlm("./1.txt")
+    res = 0
+    for i = 4:length(arr)
+        if arr[i] > arr[i - 3]
+            res += 1
+        end
+    end
+    return res
+
+end
+
+print(solve())
+    
