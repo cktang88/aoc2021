ea1a539f361e756dc7cd2a9596e44807b79c30e5
cktang88 <arctangent20@yahoo.com>
2021-12-04 12:36:54 -0600

    i

---
+++ 2.jl
@@ -0,0 +1,25 @@
+using DelimitedFiles
+
+
+function solve()
+    arr = readdlm("./2.txt") # reads into matrix
+    words, dist = eachcol(arr) # views
+    
+    horiz, aim, depth = 0, 0, 0
+    for i = 1:length(words)
+        if words[i] == "down"
+            aim += dist[i]
+        elseif words[i] == "forward"
+            horiz += dist[i]
+            depth += aim * dist[i]
+        elseif words[i] == "up"
+            aim -= dist[i]
+        end
+    end
+    
+    return horiz * depth
+
+end
+
+print(solve())
+    
--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # aoc2021
 
-
 Done in Julia b/c high perf and very expressive
+
+## Resources
+
+https://juliabyexample.helpmanual.io/
+
+https://syl1.gitbook.io/julia-language-a-concise-tutorial
+
+https://learnxinyminutes.com/docs/julia/
+
+https://docs.julialang.org
